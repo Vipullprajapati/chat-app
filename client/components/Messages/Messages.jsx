3dbaf9f792bbb153fdf1755d12f7c79819cc3368
Vipul Prajapati <vipulprajapati0070@gmail.com>
2025-02-19 06:46:32 +0000

    update in navbar

--- client/components/Messages/Messages.jsx
+++ client/components/Messages/Messages.jsx
@@ -39,32 +39,29 @@ export default function Messages({ messages, id }) {
   return (
     <>
       {/* {  // navbar} */}
-      <div className="bg-blue-400 text-white  w-full">
-        <div className="container flex flex-row justify-between items-center sm:px-5 py-2 px-2">
-          {" "}
-          {/* Changed justify-space-between to justify-between>
-          {/* Back button */}
-          <div className="flex items-center">
-            <ArrowLeft
-              className="w-5 h-5 cursor-pointer"
-              aria-label="Go back"
-            />
-            {/* Team network */}
-            <h1 className="flex gap-2">Network : {useOnlineStatus ? "online" : "offline"}</h1>
-            <ul className="flex flex-row gap-2 items-center">
-              <li className="flex items-center gap-6">
-                {/* Changed <div> to <li> for semantic correctness */}
-                <Video
-                  className="w-5 h-5 cursor-pointer"
-                  aria-label="Start video call"
-                />
-                <Phone
-                  className="w-5 h-5 cursor-pointer"
-                  aria-label="Start audio call"
-                />
-              </li>
-            </ul>
+      {/* Back button */}
+      <div className="flex  justify-content-space-between bg-gradient-to-r from-green-200 to-gray-100 border border-green-200 rounded-lg shadow-md animate-slide-in max-w-100% px-5 py-3 gap-2">
+        <div className="flex items-center ">
+        <ArrowLeft className="w-5 h-5 cursor-pointer" aria-label="Go back" />
+          <h1 className="flex gap-2">
+            Network : {useOnlineStatus ? "online" : "offline"}
+          </h1>
           </div>
+
+          <div className="flex  items-center  justify-content-space-between bg-gradient-to-r from-green-200 to-gray-100 border border-green-200 rounded-lg shadow-md animate-slide-in max-w-100% px-5 py-3 gap-2">
+          <ul className="flex flex-row gap-2 items-center">
+            <li className="flex items-center gap-6">
+              {/* Changed <div> to <li> for semantic correctness */}
+              <Video
+                className="w-5 h-5 cursor-pointer"
+                aria-label="Start video call"
+              />
+              <Phone
+                className="w-5 h-5 cursor-pointer"
+                aria-label="Start audio call"
+              />
+            </li>
+          </ul>
         </div>
       </div>
 
